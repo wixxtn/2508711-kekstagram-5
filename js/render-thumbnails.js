cdeb3ef624be711c202170c8ab1c87fdfe472ea7
Ульяна <ul.1.aannnaaaa@gmail.com>
2024-12-06 01:39:47 +0500

    Открывается и закрывается

--- js/render-thumbnails.js
+++ js/render-thumbnails.js
@@ -1,19 +1,24 @@
+import { renderBigPicture } from './full-picture.js';
+
 export const renderThumbnails = (photos) => {
   const template = document.querySelector('#picture').content.querySelector('.picture');
   const container = document.querySelector('.pictures');
 
   const fragment = document.createDocumentFragment();
 
-  photos.forEach(({ url, description, likes, comments }) => {
+  photos.forEach((photo) => {
     const element = template.cloneNode(true);
 
-    element.querySelector('.picture__img').src = url;
-    element.querySelector('.picture__img').alt = description;
-    element.querySelector('.picture__likes').textContent = likes;
-    element.querySelector('.picture__comments').textContent = comments.length;
+    element.querySelector('.picture__img').src = photo.url;
+    element.querySelector('.picture__img').alt = photo.description;
+    element.querySelector('.picture__likes').textContent = photo.likes;
+    element.querySelector('.picture__comments').textContent = photo.comments.length;
+
+    element.addEventListener('click', () => {
+      renderBigPicture(photo);
+    });
 
     fragment.appendChild(element);
-
   });
 
   container.appendChild(fragment);
